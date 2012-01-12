16759d0f456f09e1a8fa73f7ade4731819517ddf
Amol Jadhav <amolj.1306@gmail.com>
2012-01-12 20:31:45 +0530

    modified in office

--- ggson.r
+++ ggson.r
@@ -1,70 +1,197 @@
 #!/usr/bin/Rscript
-timeseries = function(data, factor_col) {
+library('stringr')
+library('plyr')
+library('ggplot2')
+library('reshape')
+source('reader.sar.r')
+generate.facet = function(data, factor_col, reverse=F, mncr=4) {
     if (is.null(factor_col)) factor_col = '.'
-
-    p = ggplot(data, aes(Time, value)) + geom_point() + geom_smooth()
-    if (length(levels(data$variable)) == 1)
-        p = p + geom_facets(facets=paste(factor_col, 'test.name', sep='~'))
+    if (length(levels(data$variable)) == 1 && factor_col == '.')
+        facets = paste(factor_col, 'test.name', sep='~')
+    else if (length(levels(data$variable)) > 1 && factor_col == '.' && length(levels(data$test.name)) > 1)
+        facets = paste('variable', factor_col, sep='~')
     else if (factor_col != '.' & length(levels(data$test.name)) == 1)
-        p = p + geom_facets(facets=paste('variable', factor_col, sep='~'))
+        facets = paste('variable', factor_col, sep='~')
     else if (factor_col != '.')
-        p = p + geom_facets(facets=paste(paste('variable+', factor_col, sep=''), 'test.name', sep='~'))
+        facets = paste('variable', paste('test.name+', factor_col, sep=''), sep='~')
     else
-        p = p + geom_facets(facets=paste('variable', 'test.name', sep='~'))
+        facets = paste('variable', 'test.name', sep='~')
+
+    parts = str_split(facets, '~')[[1]]
+    if (reverse) {
+        facets = paste(parts[2], parts[1], sep='~')
+    }
+
+    if (length(grep(parts, pattern='^\\.$')) == 0) 
+        return (facet_grid(facets))
+
+    parts = str_split(facets, '~')[[1]]
+    if (parts[1] == '.') {
+        mncr = as.integer(length(levels(data[[parts[2]]])) / 2)
+        f = facet_wrap(parts[2], ncol=mncr)
+    } else {
+        mncr = as.integer(length(levels(data[[parts[1]]])) / 2)
+        f = facet_wrap(parts[1], nrow=mncr)
+    }
+}
+
+
+boxplt = function(data, pdesc) {
+    dofacet = T
+    if (is.null(pdesc$category)) {
+        pdesc$category = 'test.name'
+        dofacet = F
+    }
+
+    p = ggplot(data) + geom_boxplot(aes_string(x=pdesc$category, y='value'))
+    if (dofacet  && (length(levels(data[['variable']])) > 1 ||length(levels(data[[pdesc$category]])) > 1 || length(levels(data[['test.name']])) > 1))
+        p = p + generate.facet(data, NULL)
     return (p)
 }
 
-open_device  = function(gdesc, fileprefix) {
-    file.name = paste(gdesc, fileprefix, '.png', sep='')
+boxpltflp = function(data, pdesc) {
+    if (is.null(pdesc$category))
+        pdesc$category = 'test.name'
+    p = ggplot(data) + geom_boxplot(aes_string(x=pdesc$category, y='value'))
+    if (length(levels(data[['variable']])) > 1 ||length(levels(data[[pdesc$category]])) > 1 || length(levels(data[['test.name']])) > 1)
+        p = p + generate.facet(data, NULL, reverse=T)
+}
+
+jit = function(data, pdesc) {
+    dofacet = T
+    if (is.null(pdesc$category)) {
+        pdesc$category = 'test.name'
+        dofacet = F
+    }
+
+    if (is.null(pdesc$alpha)) pdesc$alpha = 1
+    p = ggplot(data) + geom_jitter(aes_string(x=pdesc$category, y='value'), alpha=I(1/pdesc$alpha))
+    if (dofacet && (length(levels(data[['variable']])) > 1 ||length(levels(data[[pdesc$category]])) > 1 || length(levels(data[['test.name']])) > 1))
+        p = p + generate.facet(data, NULL) 
+}
+
+is.facetable = function(data, category) {
+    length(levels(data[['variable']])) > 1 ||length(levels(data[[category]])) > 1 || length(levels(data[['test.name']])) > 1
+}
+
+boxjit = function(data, pdesc) {
+
+    dofacet = T
+    if (is.null(pdesc$category)) {
+        pdesc$category = 'test.name'
+        dofacet = F
+    }
+
+    print(pdesc$category)
+    if (is.null(pdesc$alpha)) pdesc$alpha = 1
+    p = ggplot(data, aes_string(x=pdesc$category, y='value')) + geom_boxplot() + geom_jitter(alpha=I(1/pdesc$alpha)) 
+    if (dofacet && is.facetable(data, pdesc$category))
+            p = p + generate.facet(data, NULL)+ opts(axis.text.x=theme_text(angle=90))
+    else p
+}
+
+timeseries = function(data, pdesc) {
+    if (is.null(pdesc$alpha)) pdesc$alpha = 1
+    if (is.null(pdesc$category)) pdesc$category = '.'
+    if (!is.null(pdesc$color))
+        p = ggplot(data, aes(Time, value)) + geom_point(aes_string(color=pdesc$color), alpha=I(1/pdesc$alpha)) + geom_smooth(aes_string(color=pdesc$color))
+    else
+        p = ggplot(data, aes(Time, value)) + geom_point(alpha=I(1/pdesc$alpha)) + geom_smooth()
+    if (length(levels(data[['variable']])) > 1 ||length(levels(data[[pdesc$category]])) > 1 || length(levels(data[['test.name']])) > 1)
+        p = p + generate.facet(data, pdesc$category) 
+    p + scale_x_datetime(format="%H:%M:%S") + opts(axis.text.x=theme_text(angle=90))
+}
+
+open_device  = function(gdesc, fileprefix, height, width) {
+    if (is.null(height)) height = 600
+    if (is.null(width)) width = 800
+    file.name = paste(fileprefix, gdesc, '.png', sep='')
     fl = paste(out.location, file.name, sep='/')
     png(fl, height=height, width=width)
 }
 
-process.plot = function(gdesc, data, factor_col, fileprefix, Title) {
+mysummary = function(x, ...) {
+    r = summary(as.numeric(x), ...)
+    if (length(grep("NA's", names(r), ignore.case=T)) == 0)
+        r[["NA's"]] = 0
+    r2 = as.data.frame(r) 
+    names(r2) = names(r)
+    return (r2)
+}
+summarize.data = function(data, pdesc) {
+    file = paste(out.location, paste(pdesc$fileprefix, 'summary', sep=''), sep='/')
+    d = ddply(data[,-1], .(variable, test.name), summary)
+    write.csv(d, file=file)
+}
+
+process.plot = function(gdesc, pdesc) {
     f = get(gdesc)
-    open_device(gdesc, fileprefix)
-    plt = f(data, factor_col)
-    plt + opt(title='Title')
+    open_device(gdesc, pdesc$fileprefix, pdesc$height, pdesc$width)
+    plt = f(pdesc$data, pdesc)
+    if (!is.null(pdesc$yscale)) {
+        scale = paste('scale_y', pdesc$yscale, sep='_')
+        f = get(scale)
+        plt  = plt + f()
+    }
+    if (!is.null(pdesc$Title))
+        plt = plt + opts(title=pdesc$Title)
+    print("creating graph")
     print(plt)
     dev.off()
 }
 
-build_condition(x, values sep='==', condition='|') {
+build_condition = function(x, values, sep='==', condition='|') {
     paste(paste(x, values, sep=sep), sep=condition)
 }
 
-do.trans = function(d, data) {
-    x = data[[d$variable]]
-    x = eval(parse(text=d$trans))
-}
-
 modify = function(data,group,trans) {
-    trans = c(trans, rep('x', times=length(group) - length(trans))
+    trans = c(trans, rep('x', times=length(group) - length(trans)))
     d = data.frame(variable=group, trans=trans, stringsAsFactors=F) 
-    transfrmed = ddply(d, .(variable), .fun=do.trans, data)
+    #transfrmed = ddply(d, .(variable), .fun=do.trans, data)
+    for (i in 1:nrow(d)) {
+        x = data[[d[i,1]]]
+        data[[as.character(d[i, 1])]] = eval(parse(text=as.character(d[i, 2])))
+    }
+    return (data)
 }
 
-process.graphs = function(pdesc, factor_col, data) {
-    basic = c('Time', factor_col, 'test.name')
+ggrep = function(pattern, x) {
+    grep(pattern, x)
+}
+
+process.graphs = function(pdesc, ddesc) {
+    basic = c('Time', ddesc$category, 'test.name')
     sel = c(basic, pdesc$group)
-    groups = build_condition('names(data)',  sel, sep='==', condition='|')
-    data = data[, eval(groups)]
-    data = modify(data, pdesc$group, pdesc$trans)
-    if (!is.null(pdesc$alias) & length(pdesc$alias) == length(pdesc$group))
-        names(data) = c(basic, pdesc$alias)
-    data = melt(data, id=basic)
-    l_ply(pdesc$graphs, process.plot, data, factor_col, pdesc$fileprefix, pdesc$Title)
+    pdesc$data = ddesc$data[, sel]
+    pdesc$data = modify(pdesc$data, pdesc$group, pdesc$trans)
+    if (!is.null(pdesc$alias)) {
+        if (length(pdesc$alias) < length(pdesc$group))
+            pdesc$alias = c(pdesc$alias, pdesc$group[length(pdesc$alias)+1:length(pdesc$group)])
+        names(pdesc$data) = c(basic, pdesc$alias[!is.na(pdesc$alias)])
+    }
+    pdesc$data = melt(pdesc$data, id=basic)
+    pdesc$category = ddesc$category
+    l_ply(pdesc$graphs, process.plot, .progress='text', pdesc)
+}
+
+read.data = function(file) {
+    test.name = basename(dirname(file))
+    data = read.sar.file(file, elapsed=T)
+    data$test.name = factor(test.name)
+    return (data)
 }
 
 data.processor = function(ddesc, path) {
-    files = list.files(path=path, ddesc$pattern)
-    data = ldply(files, read.data)
-    ddesc$category
+    files = list.files(path=path, ddesc$pattern, recursive=T, full.names=T)
+    print(files)
+    ddesc$data = ldply(files, read.data)
     if (!is.null(ddesc$exclude) & length(ddesc$exclude) != 0) {
-        excludes = build_condition(paste('data', ddesc$category, sep='$'), ddesc$exclude, sep='!=')
-        data = data[eval(excludes),] 
+        ddesc$exclude = paste("'", ddesc$exclude, "'", sep='')
+        excludes = build_condition(paste('ddesc$data', ddesc$category, sep='$'), ddesc$exclude, sep='!=', condition='&')
+        ddesc$data = ddesc$data[eval(parse(text=excludes)),] 
     }
     nms = names(ddesc)
-    processors = nms != 'category' & nms != 'pattern' & nms != 'exclude'
-    l_aply(ddesc[processor], process.graphs, data)
+    processors = nms != 'category' & nms != 'pattern' & nms != 'exclude' & nms != 'data'
+    sq = 1:length(ddesc)
+    l_ply(ddesc[ sq[processors] ], process.graphs, .progress='text', ddesc)
 }
